4147371d488e13aca4f8892946690af4967906ee
Niels Steenbergen <n.steenbergen@uu.nl>
2021-01-26 13:03:02 +0100

    Change variable names for readability.

--- quangis/transformation/algebra.py
+++ quangis/transformation/algebra.py
@@ -20,7 +20,7 @@ x, y, z = (TypeVar() for _ in range(0, 3))
 
 class TransformationAlgebra(ABC):
     """
-    Generic abstract transformation algebra. To make a particular
+    Abstract base for transformation algebras. To make a concrete
     transformation algebra, subclass this class and add a mapping "functions"
     of function names to corresponding AlgebraTypes.
     """
@@ -41,49 +41,46 @@ class CCT(TransformationAlgebra):
     """
     Core concept transformation algebra.
     """
-    # TODO if we rename some transformations, we can simply make them all
-    # attributes of the class, which would make it easy to allow us to define
-    # transformations in terms of other transformations
 
-    # Entity types
-    Entity = partial(TypeOperator)
-    V = Entity("entity")
-    O = Entity("object", supertype=V)  # type: ignore
-    S = Entity("region", supertype=V)
-    L = Entity("location", supertype=V)
-    Q = Entity("quality", supertype=V)
-    Nom = Entity("nominal", supertype=Q)
-    Bool = Entity("boolean", supertype=Nom)
-    Ord = Entity("ordinal", supertype=Nom)
-    Count = Entity("count", supertype=Ord)
-    Ratio = Entity("ratio", supertype=Count)
-    Itv = Entity("interval", supertype=Ratio)
+    # Entity value types
+    V = partial(TypeOperator)
+    Ent = V("entity")
+    Obj = V("object", supertype=Ent)  # O
+    Reg = V("region", supertype=Ent)  # S
+    Loc = V("location", supertype=Ent)  # L
+    Qlt = V("quality", supertype=Ent)  # Q
+    Nom = V("nominal", supertype=Qlt)
+    Bool = V("boolean", supertype=Nom)
+    Ord = V("ordinal", supertype=Nom)
+    Count = V("count", supertype=Ord)
+    Ratio = V("ratio", supertype=Count)
+    Itv = V("interval", supertype=Ratio)
 
     # Relation types and type synonyms
     R = partial(TypeOperator, "R")
-    SpatialField = R(L, Q)
-    InvertedField = R(Q, S)
-    FieldSample = R(S, Q)
-    ObjectExtent = R(O, S)
-    ObjectQuality = R(O, Q)
-    NominalField = R(L, Nom)
-    BooleanField = R(L, Bool)
-    NominalInvertedField = R(Nom, S)
-    BooleanInvertedField = R(Bool, S)
+    SpatialField = R(Loc, Qlt)
+    InvertedField = R(Qlt, Reg)
+    FieldSample = R(Reg, Qlt)
+    ObjectExtent = R(Obj, Reg)
+    ObjectQuality = R(Obj, Qlt)
+    NominalField = R(Loc, Nom)
+    BooleanField = R(Loc, Bool)
+    NominalInvertedField = R(Nom, Reg)
+    BooleanInvertedField = R(Bool, Reg)
 
     functions = {
 
         # data constructors
-        "pointmeasures": R(S, Itv),
-        "amountpatches": R(S, Nom),
-        "contour": R(Ord, S),
-        "objects": R(O, Ratio),
-        "objectregions": R(O, S),
-        "contourline": R(Itv, S),
-        "objectcounts": R(O, Count),
-        "field": R(L, Ratio),
-        "object": R(O),
-        "region": R(S),
+        "pointmeasures": R(Reg, Itv),
+        "amountpatches": R(Reg, Nom),
+        "contour": R(Ord, Reg),
+        "objects": R(Obj, Ratio),
+        "objectregions": R(Obj, Reg),
+        "contourline": R(Itv, Reg),
+        "objectcounts": R(Obj, Count),
+        "field": R(Loc, Ratio),
+        "object": R(Obj),
+        "region": R(Reg),
         "in": R(Nom),
         "countV": R(Count),
         "ratioV": R(Ratio),
@@ -100,37 +97,37 @@ class CCT(TransformationAlgebra):
         "ratio": Ratio ** Ratio ** Ratio,
 
         # aggregations of collections
-        "count": R(O) ** Ratio,
-        "size": R(L) ** Ratio,
-        "merge": R(S) ** S,
-        "centroid": R(L) ** L,
+        "count": R(Obj) ** Ratio,
+        "size": R(Loc) ** Ratio,
+        "merge": R(Reg) ** Reg,
+        "centroid": R(Loc) ** Loc,
 
         # statistical operations
-        "avg": R(V, Itv) ** Itv,
-        "min": R(V, Ord) ** Ord,
-        "max": R(V, Ord) ** Ord,
-        "sum": R(V, Count) ** Count,
+        "avg": R(Ent, Itv) ** Itv,
+        "min": R(Ent, Ord) ** Ord,
+        "max": R(Ent, Ord) ** Ord,
+        "sum": R(Ent, Count) ** Count,
 
         # conversions
-        "reify": R(L) ** S,
-        "deify": S ** R(L),
-        "get": R(x) ** x | {x: Sub(V)},
-        "invert": R(L, Ord) ** R(Ord, S),  # TODO overload R(L, Nom) ** R(S, Nom)
-        "revert": R(Ord, S) ** R(L, Ord),  # TODO overload
+        "reify": R(Loc) ** Reg,
+        "deify": Reg ** R(Loc),
+        "get": R(x) ** x | {x: Sub(Ent)},
+        "invert": R(Loc, Ord) ** R(Ord, Reg),  # TODO overload R(Loc, Nom) ** R(Reg, Nom)
+        "revert": R(Ord, Reg) ** R(Loc, Ord),  # TODO overload
 
         # quantified relations
-        "oDist": R(O, S) ** R(O, S) ** R(O, Ratio, O),
-        "lDist": R(L) ** R(L) ** R(L, Ratio, L),
-        "loDist": R(L) ** R(O, S) ** R(L, Ratio, O),
-        "oTopo": R(O, S) ** R(O, S) ** R(O, Nom, O),
-        "loTopo": R(L) ** R(O, S) ** R(L, Nom, O),
-        "nDist": R(O) ** R(O) ** R(O, Ratio, O) ** R(O, Ratio, O),
-        "lVis": R(L) ** R(L) ** R(L, Itv) ** R(L, Bool, L),
-        "interpol": R(S, Itv) ** R(L) ** R(L, Itv),
+        "oDist": R(Obj, Reg) ** R(Obj, Reg) ** R(Obj, Ratio, Obj),
+        "lDist": R(Loc) ** R(Loc) ** R(Loc, Ratio, Loc),
+        "loDist": R(Loc) ** R(Obj, Reg) ** R(Loc, Ratio, Obj),
+        "oTopo": R(Obj, Reg) ** R(Obj, Reg) ** R(Obj, Nom, Obj),
+        "loTopo": R(Loc) ** R(Obj, Reg) ** R(Loc, Nom, Obj),
+        "nDist": R(Obj) ** R(Obj) ** R(Obj, Ratio, Obj) ** R(Obj, Ratio, Obj),
+        "lVis": R(Loc) ** R(Loc) ** R(Loc, Itv) ** R(Loc, Bool, Loc),
+        "interpol": R(Reg, Itv) ** R(Loc) ** R(Loc, Itv),
 
         # amount operations
-        "fcont": R(L, Itv) ** Ratio,
-        "ocont": R(O, Ratio) ** Ratio,
+        "fcont": R(Loc, Itv) ** Ratio,
+        "ocont": R(Obj, Ratio) ** Ratio,
 
         # relational
         "pi1":
@@ -140,17 +137,17 @@ class CCT(TransformationAlgebra):
         "pi3":
             x ** y | {y: Contains(x, at=3)},
         "sigmae":
-            x ** y ** x | {x: Sub(Q), y: Contains(x)},
+            x ** y ** x | {x: Sub(Qlt), y: Contains(x)},
         "sigmale":
             x ** y ** x | {x: Sub(Ord), y: Contains(x)},
         "bowtie":
-            x ** R(y) ** x | {y: Sub(V), x: Contains(y)},
+            x ** R(y) ** x | {y: Sub(Ent), x: Contains(y)},
         "bowtie*":
-            R(x, y, x) ** R(x, y) ** R(x, y, x) | {y: Sub(Q), x: Sub(V)},
+            R(x, y, x) ** R(x, y) ** R(x, y, x) | {y: Sub(Qlt), x: Sub(Ent)},
         "bowtie_":
-            (Q ** Q ** Q) ** R(V, Q) ** R(V, Q) ** R(V, Q),
+            (Qlt ** Qlt ** Qlt) ** R(Ent, Qlt) ** R(Ent, Qlt) ** R(Ent, Qlt),
         "groupbyL":
-            (R(y, Q) ** Q) ** R(x, Q, y) ** R(x, Q) | {x: Sub(V), y: Sub(V)},
+            (R(y, Qlt) ** Qlt) ** R(x, Qlt, y) ** R(x, Qlt) | {x: Sub(Ent), y: Sub(Ent)},
         "groupbyR":
-            (R(x, Q) ** Q) ** R(x, Q, y) ** R(y, Q) | {x: Sub(V), y: Sub(V)},
+            (R(x, Qlt) ** Qlt) ** R(x, Qlt, y) ** R(y, Qlt) | {x: Sub(Ent), y: Sub(Ent)},
     }
