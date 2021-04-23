a534fa2daf0d180ffedab7dcefff903cfd66b925
Niels Steenbergen <n.steenbergen@uu.nl>
2021-04-23 18:07:26 +0200

    Self-contained tests for expressions too.

--- tests/test_expr.py
+++ tests/test_expr.py
@@ -5,29 +5,23 @@ from transformation_algebra.type import Type
 from transformation_algebra.expr import \
     TransformationAlgebra, Data, Operation
 
-Int = Type.declare('Int')
-
-one = Data(Int)
-add = Operation(Int ** Int ** Int)
-add1 = Operation(
-    Int ** Int,
-    derived=lambda x: add(x, one)
-)
-compose = Operation(
-    lambda α, β, γ: (β ** γ) ** (α ** β) ** (α ** γ),
-    derived=lambda f, g, x: f(g(x))
-)
-algebra = TransformationAlgebra(**globals())
-
-
-A = Type.declare('A')
-B = Type.declare('B')
-f = Operation(A ** B)
-g = Operation(lambda α: α ** B)
-
 
 class TestAlgebra(unittest.TestCase):
     def test_primitive(self):
+        Int = Type.declare('Int')
+        add = Operation(Int ** Int ** Int)
+        one = Data(Int)
+        algebra = TransformationAlgebra(
+            one=one,
+            add1=Operation(
+                Int ** Int,
+                derived=lambda x: add(x, one)
+            ),
+            compose=Operation(
+                lambda α, β, γ: (β ** γ) ** (α ** β) ** (α ** γ),
+                derived=lambda f, g, x: f(g(x))
+            )
+        )
         t = algebra.parse("compose add1 add1 one")
         # TODO should be equal to:
         # Int
@@ -51,6 +45,8 @@ class TestAlgebra(unittest.TestCase):
         #  └─╼ one : Int
 
     def test_exact_declared_type_in_definition(self):
+        A, B = Type.declare('A'), Type.declare('B')
+        f = Operation(A ** B)
         self.assertRaises(
             error.SubtypeMismatch,
             Operation.validate,
@@ -59,10 +55,14 @@ class TestAlgebra(unittest.TestCase):
         Operation(A ** B, derived=lambda x: f(x)).validate()
 
     def test_tighter_declared_type_in_definition(self):
+        A, B = Type.declare('A'), Type.declare('B')
+        g = Operation(lambda α: α ** B)
         Operation(A ** B, derived=lambda x: g(x)).validate()
         Operation(B ** B, derived=lambda x: g(x)).validate()
 
     def test_looser_declared_type_in_definition(self):
+        A, B = Type.declare('A'), Type.declare('B')
+        f, g = Operation(A ** B), Operation(lambda α: α ** B)
         Operation(lambda α: α ** B, derived=lambda x: g(x)).validate()
         self.assertRaises(
             error.DeclaredTypeTooGeneral,
@@ -71,18 +71,13 @@ class TestAlgebra(unittest.TestCase):
 
     def test_same_labels_unify(self):
         # See issue #10
-        A = Type.declare("A")
-        B = Type.declare("B")
+        A, B = Type.declare('A'), Type.declare('B')
         algebra = TransformationAlgebra(
             d1=Data(A),
             d2=Data(B),
             f=Operation(A ** B ** A))
         algebra.parse("f (d1 x) (d2 y)")
-        self.assertRaises(
-            error.TATypeError,
-            algebra.parse,
-            "f (d1 x) (d2 x)"
-        )
+        self.assertRaises(error.TATypeError, algebra.parse, "f (d1 x) (d2 x)")
 
 
 if __name__ == '__main__':
