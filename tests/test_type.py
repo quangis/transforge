ad7d524d05f0a6cd9357da33ea135d52c759c34f
Niels Steenbergen <n.steenbergen@uu.nl>
2021-04-23 17:51:34 +0200

    Make tests self-contained.

--- tests/test_type.py
+++ tests/test_type.py
@@ -4,18 +4,6 @@ from transformation_algebra import error
 from transformation_algebra.type import \
     Type, TypeSchema, TypeVar, _
 
-τ = Type.declare
-
-Any = Type.declare('Any')
-Ord = Type.declare('Ord', supertype=Any)
-Bool = Type.declare('Bool', supertype=Ord)
-Str = Type.declare('Str', supertype=Ord)
-Int = Type.declare('Int', supertype=Ord)
-UInt = Type.declare('UInt', supertype=Int)
-T = Type.declare('T', 1)
-Set = Type.declare('Set', 1)
-Map = Type.declare('Map', 2)
-
 
 class TestType(unittest.TestCase):
 
@@ -36,127 +24,161 @@ class TestType(unittest.TestCase):
             f.apply(x)
 
     def test_apply_non_function(self):
-        self.apply(Int, Int, error.FunctionApplicationError)
+        A = Type.declare('A')
+        self.apply(A, A, error.FunctionApplicationError)
 
     def test_basic_match(self):
-        f = Int ** Str
-        self.apply(f, Int, Str)
+        A, B = Type.declare('A'), Type.declare('B')
+        f = A ** B
+        self.apply(f, A, B)
 
     def test_basic_mismatch(self):
-        f = Int ** Str
-        self.apply(f, Str, error.SubtypeMismatch)
+        A, B = Type.declare('A'), Type.declare('B')
+        f = A ** B
+        self.apply(f, B, error.SubtypeMismatch)
 
     def test_basic_sub_match(self):
-        f = Any ** Str
-        self.apply(f, Int, Str)
+        A = Type.declare('A')
+        B, C = Type.declare('B', supertype=A), Type.declare('C', supertype=A)
+        f = A ** C
+        self.apply(f, B, C)
 
     def test_basic_sub_mismatch(self):
-        f = Int ** Str
-        self.apply(f, Any, error.SubtypeMismatch)
+        A = Type.declare('A')
+        B, C = Type.declare('B', supertype=A), Type.declare('C', supertype=A)
+        f = B ** C
+        self.apply(f, A, error.SubtypeMismatch)
 
     def test_compound_match(self):
-        f = T(Int) ** Str
-        self.apply(f, T(Int), Str)
+        F = Type.declare('F', params=1)
+        A, B = Type.declare('A'), Type.declare('B')
+        f = F(A) ** B
+        self.apply(f, F(A), B)
 
     def test_compound_mismatch(self):
-        f = T(Int) ** Str
-        self.apply(f, T(Str), error.SubtypeMismatch)
+        F = Type.declare('F', params=1)
+        A, B = Type.declare('A'), Type.declare('B')
+        f = F(A) ** B
+        self.apply(f, F(B), error.SubtypeMismatch)
 
     def test_compound_sub_match(self):
-        f = T(Any) ** Str
-        self.apply(f, T(Int), Str)
+        A = Type.declare('A')
+        B, C = Type.declare('B', supertype=A), Type.declare('C', supertype=A)
+        F = Type.declare('F', params=1)
+        f = F(A) ** C
+        self.apply(f, F(B), C)
 
     def test_compound_sub_mismatch(self):
-        f = T(Int) ** Str
-        self.apply(f, T(Any), error.SubtypeMismatch)
+        F = Type.declare('F', params=1)
+        A = Type.declare('A')
+        B, C = Type.declare('B', supertype=A), Type.declare('C', supertype=A)
+        f = F(B) ** C
+        self.apply(f, F(A), error.SubtypeMismatch)
 
     def test_variable(self):
-        wrap = TypeSchema(lambda α: α ** T(α))
-        self.apply(wrap, Int, T(Int))
+        F = Type.declare('F', params=1)
+        A = Type.declare('A')
+        wrap = TypeSchema(lambda α: α ** F(α))
+        self.apply(wrap, A, F(A))
 
     def test_compose(self):
+        A, B = Type.declare('A'), Type.declare('B')
         compose = TypeSchema(lambda x, y, z: (y ** z) ** (x ** y) ** (x ** z))
-        self.apply(
-            compose.apply(Int ** Str), Str ** Int,
-            Str ** Str)
+        self.apply(compose.apply(A ** B), B ** A, B ** B)
 
     def test_compose_subtype(self):
+        A, B = Type.declare('A'), Type.declare('B')
+        Asub = Type.declare('Asub', supertype=A)
         compose = TypeSchema(lambda x, y, z: (y ** z) ** (x ** y) ** (x ** z))
-        self.apply(
-            compose.apply(Int ** Str), Str ** UInt,
-            Str ** Str)
+        self.apply(compose.apply(A ** B), B ** Asub, B ** B)
 
     def test_variable_subtype_mismatch(self):
-        f = TypeSchema(lambda x: (x ** Int) ** x)
-        self.apply(f, Int ** Any, error.SubtypeMismatch)
+        A = Type.declare('A')
+        B = Type.declare('B', supertype=A)
+        f = TypeSchema(lambda x: (x ** B) ** x)
+        self.apply(f, B ** A, error.SubtypeMismatch)
 
     def test_functions_as_arguments1(self):
+        A, B = Type.declare('A'), Type.declare('B')
         swap = TypeSchema(lambda α, β, γ: (α ** β ** γ) ** (β ** α ** γ))
-        f = TypeSchema(lambda x: Bool ** x ** x)
-        self.apply(swap.apply(f).apply(UInt), Bool, UInt)
+        f = TypeSchema(lambda x: A ** x ** x)
+        self.apply(swap.apply(f).apply(B), A, B)
 
     def test_functions_as_arguments2(self):
+        A = Type.declare('A')
+        B = Type.declare('B', supertype=A)
         id = TypeSchema(lambda x: x ** x)
-        f = Int ** Int
-        x = UInt
-        self.apply(id.apply(f), x, Int)
+        self.apply(id.apply(A ** A), B, A)
 
     def test_order_of_subtype_application(self):
-        """
-        This test is inspired by Traytel et al (2011).
-        """
-        leq = TypeSchema(lambda α: α ** α ** Bool)
-        self.apply(leq.apply(UInt), Int(), Bool())
-        self.apply(leq.apply(Int), UInt, Bool)
-        self.apply(leq.apply(Int), Bool, error.SubtypeMismatch)
+        # This test is inspired by Traytel et al (2011).
+        Basic = Type.declare('Basic')
+        Sub = Type.declare('Sub', supertype=Basic)
+        Other = Type.declare('Other')
+        f = TypeSchema(lambda α: α ** α ** Other)
+        self.apply(f.apply(Sub), Basic, Other)
+        self.apply(f.apply(Basic), Sub, Other)
+        self.apply(f.apply(Basic), Other, error.SubtypeMismatch)
 
     def test_order_of_subtype_application_with_constraints(self):
-        leq = TypeSchema(lambda α: α ** α ** Bool | α @ [Ord, Bool])
-        self.apply(leq.apply(Int), UInt, Bool)
-        self.apply(leq, Any, error.ConstraintViolation)
+        Super = Type.declare('Super')
+        Basic = Type.declare('Basic', supertype=Super)
+        Sub = Type.declare('Sub', supertype=Basic)
+        Other = Type.declare('Other')
+        f = TypeSchema(lambda α: α ** α ** Other | α @ [Super, Basic])
+        self.apply(f.apply(Basic), Sub, Other)
+        self.apply(f, Super, error.ConstraintViolation)
 
     def test_violation_of_constraints(self):
-        sum = TypeSchema(lambda α: α ** α | α @ [Int, Set(Int)])
-        self.apply(sum, Set(UInt), Set(UInt))
-        self.apply(sum, Bool, error.ConstraintViolation)
+        F = Type.declare('F', params=1)
+        A = Type.declare('A')
+        B = Type.declare('B', supertype=A)
+        C = Type.declare('C')
+        f = TypeSchema(lambda α: α ** α | α @ [A, F(A)])
+        self.apply(f, F(B), F(B))
+        self.apply(f, C, error.ConstraintViolation)
 
     def test_preservation_of_basic_subtypes_in_constraints(self):
-        f = TypeSchema(lambda x: x ** x | x @ [Any])
-        self.apply(f, Int, Int)
+        Super = Type.declare('Super')
+        Basic = Type.declare('Basic', supertype=Super)
+        f = TypeSchema(lambda x: x ** x | x @ [Super])
+        self.apply(f, Basic, Basic)
 
     def test_unification_of_compound_types_in_constraints(self):
-        f = TypeSchema(lambda xs, x: xs ** x | xs @ [Set(x), T(x)])
-        self.apply(f, T(Int), Int)
+        F, G = Type.declare('F', params=1), Type.declare('G', params=1)
+        A = Type.declare('A')
+        f = TypeSchema(lambda xs, x: xs ** x | xs @ [G(x), F(x)])
+        self.apply(f, F(A), A)
 
     def test_non_unification_of_base_types(self):
-        """
-        We cannot unify with base types from constraints, because they might
-        also be subtypes. So in this case, we know that x is a Map, but we
-        don't know that its parameters are exactly Str and Int: that might be
-        too loose a bound.
-        """
-        f = TypeSchema(lambda x: x ** x | x @ [Map(Str, Int)])
+        # We can't unify with base types from constraints, as they might be
+        # subtypes. So in this case, we know that x is an F, but we don't know
+        # that its parameters is exactly A: that might be too general a bound.
+        F, A = Type.declare('F', params=1), Type.declare('A')
+        f = TypeSchema(lambda x: x ** x | x @ F(A))
         result = f.apply(TypeVar())
-        self.assertEqual(result.operator, Map)
+        self.assertEqual(result.operator, F)
+        self.assertTrue(isinstance(result.params[0], TypeVar))
 
     def test_multiple_bounds1(self):
-        """
-        This works because UInt ** UInt is acceptable for Int ** UInt.
-        """
+        # This works because B ** B is acceptable for A ** B.
+        A = Type.declare('A')
+        B = Type.declare('B', supertype=A)
         f = TypeSchema(lambda x: (x ** x) ** x)
-        self.apply(f, Int ** UInt, UInt)
+        self.apply(f, A ** B, B)
 
     def test_multiple_bounds2(self):
-        """
-        This doesn't work because the upper bound UInt cannot be reconciled
-        with the lower bound Int.
-        """
+        # This doesn't work because the upper bound B cannot be reconciled
+        # with the lower bound A.
+        A = Type.declare('A')
+        B = Type.declare('B', supertype=A)
         f = TypeSchema(lambda x: (x ** x) ** x)
-        self.apply(f, UInt ** Int, error.SubtypeMismatch)
+        self.apply(f, B ** A, error.SubtypeMismatch)
 
     def test_constrain_wildcard(self):
+        A = Type.declare('A')
         f = TypeSchema(lambda x: x ** x | x @ [_])
-        self.apply(f, Int, Int)
+        self.apply(f, A, A)
 
     def test_constrain_free_variable(self):
         f = TypeSchema(lambda x, y, z: x ** x | y @ [x, z])
@@ -165,20 +187,27 @@ class TestType(unittest.TestCase):
         self.assertRaises(error.ConstrainFreeVariable, TypeSchema.instance, g)
 
     def test_global_subtype_resolution(self):
+        A = Type.declare('A')
+        B = Type.declare('B', supertype=A)
         f = TypeSchema(lambda x: x ** (x ** x) ** x)
-        self.apply(f.apply(UInt), Int ** UInt, UInt)
-        self.apply(f.apply(Int), Int ** UInt, Int)
+        self.apply(f.apply(B), A ** B, B)
+        self.apply(f.apply(A), A ** B, A)
 
     def test_interdependent_types(self):
-        f = TypeSchema(lambda α, β: α ** β | α @ [Set(β), Map(_, β)])
-        self.apply(f, Set(Int), Int)
-        self.apply(f, Int, error.ConstraintViolation)
+        A = Type.declare('A')
+        F, G = Type.declare('F', 1), Type.declare('G', 2)
+        f = TypeSchema(lambda α, β: α ** β | α @ [F(β), G(_, β)])
+        self.apply(f, F(A), A)
+        self.apply(f, A, error.ConstraintViolation)
 
     def test_subtyping_of_concrete_functions(self):
-        self.assertTrue(Int ** Int <= UInt ** Int)
-        self.assertTrue(Int ** Int <= Int ** Any)
-        self.assertFalse(Int ** Int <= Any ** Int)
-        self.assertFalse(Int ** Int <= Int ** UInt)
+        Super = Type.declare('Super')
+        Basic = Type.declare('Basic', supertype=Super)
+        Sub = Type.declare('Sub', supertype=Basic)
+        self.assertTrue(Basic ** Basic <= Sub ** Basic)
+        self.assertTrue(Basic ** Basic <= Basic ** Super)
+        self.assertFalse(Basic ** Basic <= Super ** Basic)
+        self.assertFalse(Basic ** Basic <= Basic ** Sub)
 
     def test_subtyping_of_variables(self):
         x = TypeVar()
@@ -187,28 +216,34 @@ class TestType(unittest.TestCase):
 
     def test_subtyping_of_variable_functions(self):
         x = TypeVar()
-        self.assertEqual(x ** Int <= UInt ** Int, None)
-        self.assertEqual(Int ** x <= Int ** Any, None)
-        self.assertEqual(Int ** Int <= x ** Int, None)
-        self.assertEqual(Int ** Int <= Int ** x, None)
+        Super = Type.declare('Super')
+        Basic = Type.declare('Basic', supertype=Super)
+        Sub = Type.declare('Sub', supertype=Basic)
+        self.assertEqual(x ** Basic <= Sub ** Basic, None)
+        self.assertEqual(Basic ** x <= Basic ** Super, None)
+        self.assertEqual(Basic ** Basic <= x ** Basic, None)
+        self.assertEqual(Basic ** Basic <= Basic ** x, None)
 
     def test_subtyping_of_wildcard_functions(self):
-        self.assertTrue(_ ** Int <= UInt ** Int)
-        self.assertTrue(Int ** _ <= Int ** Any)
-        self.assertTrue(Int ** Int <= _ ** Int)
-        self.assertTrue(Int ** Int <= Int ** _)
-        self.assertFalse(_ ** Any <= UInt ** Int)
-        self.assertFalse(UInt ** _ <= Int ** Any)
+        Super = Type.declare('Super')
+        Basic = Type.declare('Basic', supertype=Super)
+        Sub = Type.declare('Sub', supertype=Basic)
+        self.assertTrue(_ ** Basic <= Sub ** Basic)
+        self.assertTrue(Basic ** _ <= Basic ** Super)
+        self.assertTrue(Basic ** Basic <= _ ** Basic)
+        self.assertTrue(Basic ** Basic <= Basic ** _)
+        self.assertFalse(_ ** Super <= Sub ** Basic)
+        self.assertFalse(Sub ** _ <= Basic ** Super)
 
     def test_constrained_to_base_type(self):
-        # Addresses issue #2, which caused an infinite loop
+        # See issue #2, which caused an infinite loop
         A = Type.declare('A')
         f = TypeSchema(lambda x: x ** x | x @ A)
         g = TypeSchema(lambda x, y: (x ** y) ** y)
         self.apply(g, f)
 
     def test_constrained_to_compound_type(self):
-        # Same as before, making sure that compound types also work
+        # See issue #2
         A = Type.declare('A')
         F = Type.declare('F', params=1)
         f = TypeSchema(lambda x: x ** x | x @ F(A))
@@ -217,7 +252,8 @@ class TestType(unittest.TestCase):
 
     def test_unification_of_constraint_with_variables(self):
         # See issue #13
-        A, B, C, R2, R3 = τ('A'), τ('B'), τ('C'), τ('R2', 2), τ('R3', 3)
+        A, B, C = Type.declare('A'), Type.declare('B'), Type.declare('C')
+        R2, R3 = Type.declare('R2', 2), Type.declare('R3', 3)
         actual = TypeSchema(lambda x:
             R3(A, x, C) | R2(C, B) @ [R2(A, x), R2(C, x)])
         expected = R3(A, B, C)
@@ -253,8 +289,8 @@ class TestType(unittest.TestCase):
     def test_unification_of_constraint_option_subtypes(self):
         # See issue #16
         A = Type.declare('A')
-        F = Type.declare('F', params=2)
         B = Type.declare('B', supertype=A)
+        F = Type.declare('F', params=2)
         f = TypeSchema(lambda x, y: F(x, y) | F(y, A) @ [F(A, x), F(B, x)])
         actual = f.instance().params[0]
         expected = A.instance()
