a2144daba2f998e4e98dd0869dc367f042dc1c87
Niels Steenbergen <n.steenbergen@uu.nl>
2021-09-24 18:18:16 +0200

    Add test for internals.

--- tests/test_rdf.py
+++ tests/test_rdf.py
@@ -1,10 +1,15 @@
+"""
+Tests for the graph conversion process of algebra expressions. To check the
+tests, it's recommended to draw out the graphs with a pen.
+"""
+
 import unittest
 
 from rdflib import Namespace, Graph, BNode, RDF, URIRef
 from rdflib.term import Node
 from rdflib.compare import to_isomorphic, graph_diff
 
-from typing import Iterator, Dict, Optional
+from typing import Iterator, Dict, Optional, Any
 
 from transformation_algebra import error
 from transformation_algebra.type import Type
@@ -12,15 +17,19 @@ from transformation_algebra.expr import \
     Expr, TransformationAlgebra, Data, Operation
 from transformation_algebra.rdf import TransformationAlgebraRDF, TA
 
+ALG = Namespace('ALG#')
+
 
 class Step(object):
     def __init__(self,
             transformer: Optional[URIRef],
-            *inputs: int,
-            type: Optional[Type] = None):
+            *inputs: Any,
+            type: Optional[Type] = None,
+            internals: list[Any] = []):
         self.inputs = inputs
         self.type = type
         self.transformer = transformer
+        self.internals = internals
 
 
 class TestAlgebraRDF(unittest.TestCase):
@@ -50,7 +59,7 @@ class TestAlgebraRDF(unittest.TestCase):
     def compare(self,
             alg: TransformationAlgebraRDF,
             expr: Expr,
-            manual: Dict[int, Step]) -> None:
+            manual: Dict[Any, Step]) -> None:
         """
         Compare the automatically constructed graph with a manual counterpart.
         """
@@ -78,6 +87,8 @@ class TestAlgebraRDF(unittest.TestCase):
 
             expected.add((root, TA.step, nodes[i]))
             expected.add((nodes[i], RDF.type, kind))
+            for internal in step.internals:
+                expected.add((nodes[i], TA.internal, nodes[internal]))
 
             for j in step.inputs:
                 expected.add((nodes[j], TA.feeds, nodes[i]))
@@ -85,15 +96,9 @@ class TestAlgebraRDF(unittest.TestCase):
         self.assertIsomorphic(actual, expected)
 
     def test_basic(self):
-        """
-        Test that basic expressions can be turned into graphs.
-        """
-
         A = Type.declare("A")
         x = Data(A, name="x")
         f = Operation(A ** A, name="f")
-
-        ALG = Namespace('ALG#')
         alg = TransformationAlgebraRDF('alg', ALG)
         alg.add(f, x)
 
@@ -102,3 +107,19 @@ class TestAlgebraRDF(unittest.TestCase):
             {1: Step(None), 2: Step(ALG.f, 1)}
         )
 
+    def test_operation_as_parameter_with_argument(self):
+        A = Type.declare("A")
+        x = Data(A, name="x")
+        f = Operation((A ** A) ** A ** A, name="f")
+        g = Operation(A ** A, name="g")
+        alg = TransformationAlgebraRDF('alg', ALG)
+        alg.add(f, g, x)
+
+        self.compare(alg,
+            f(g, x), {
+                "x": Step(None),
+                "λ": Step(None, "x"),
+                "f": Step(ALG.f, "g", "x", internals=["λ"]),
+                "g": Step(ALG.g, "λ")
+            }
+        )
