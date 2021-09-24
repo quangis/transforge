44ac154bbf1bb619e43c8b70f9b22c40713cbe85
Niels Steenbergen <n.steenbergen@uu.nl>
2021-09-24 17:40:28 +0200

    Generalize tests.

--- tests/test_rdf.py
+++ tests/test_rdf.py
@@ -1,36 +1,93 @@
 import unittest
 
-from rdflib import Namespace, Graph, BNode, RDF
+from rdflib import Namespace, Graph, BNode, RDF, URIRef
 from rdflib.term import Node
 from rdflib.compare import to_isomorphic, graph_diff
-from rdflib.tools.rdf2dot import rdf2dot
 
-from typing import Iterator
+from typing import Iterator, Dict, Optional
 
 from transformation_algebra import error
 from transformation_algebra.type import Type
 from transformation_algebra.expr import \
-    TransformationAlgebra, Data, Operation
+    Expr, TransformationAlgebra, Data, Operation
 from transformation_algebra.rdf import TransformationAlgebraRDF, TA
 
 
-def nodes(graph: Graph) -> set[Node]:
-    return set(node for triple in graph for node in triple
-        if not isinstance(node, BNode))
-
-
-def visualize(graph: Graph, filename: str) -> None:
-    """
-    Visualize a graph. For debugging.
-    """
-
-    with open(filename, 'w') as f:
-        rdf2dot(graph, f)
+class Step(object):
+    def __init__(self,
+            transformer: Optional[URIRef],
+            *inputs: int,
+            type: Optional[Type] = None):
+        self.inputs = inputs
+        self.type = type
+        self.transformer = transformer
 
 
 class TestAlgebraRDF(unittest.TestCase):
 
+    def assertIsomorphic(self, actual: Graph, expected: Graph) -> None:
+        """
+        Compare two graphs to test if they are equal
+        """
+        actual = to_isomorphic(actual)
+        expected = to_isomorphic(expected)
+
+        nodes_expected = set(node for triple in expected for node in triple
+            if not isinstance(node, BNode))
+        nodes_actual = set(node for triple in actual for node in triple
+            if not isinstance(node, BNode))
+
+        # Test that there are no missing nodes
+        for n in nodes_expected:
+            self.assertIn(n, nodes_actual)
+
+        # Test that there are no extraneous nodes
+        self.assertFalse(nodes_actual - nodes_expected)
+
+        # If all nodes are there, there must be some wrong connection
+        self.assertEqual(actual, expected)
+
+    def compare(self,
+            alg: TransformationAlgebraRDF,
+            expr: Expr,
+            manual: Dict[int, Step]) -> None:
+        """
+        Compare the automatically constructed graph with a manual counterpart.
+        """
+        root = BNode()
+
+        # Calculate the graph from the expression
+        actual = Graph()
+        alg.rdf_expr(actual, expr, root,
+            include_labels=False, include_types=False)
+
+        # Manually construct the expected graph
+        expected = Graph()
+        nodes = {i: BNode() for i in manual}
+        expected.add((root, RDF.type, TA.Transformation))
+
+        for i, step in manual.items():
+            if step.transformer:
+                assert step.inputs
+                kind = TA.TransformedData
+                expected.add((nodes[i], TA.transformer, step.transformer))
+            elif not step.inputs:
+                kind = TA.SourceData
+            else:
+                kind = TA.InternalData
+
+            expected.add((root, TA.step, nodes[i]))
+            expected.add((nodes[i], RDF.type, kind))
+
+            for j in step.inputs:
+                expected.add((nodes[j], TA.feeds, nodes[i]))
+
+        self.assertIsomorphic(actual, expected)
+
     def test_basic(self):
+        """
+        Test that basic expressions can be turned into graphs.
+        """
 
         A = Type.declare("A")
         x = Data(A, name="x")
@@ -40,37 +97,8 @@ class TestAlgebraRDF(unittest.TestCase):
         alg = TransformationAlgebraRDF('alg', ALG)
         alg.add(f, x)
 
-        actual = Graph()
-        alg.rdf_expr(actual, f(x), root=BNode(),
-            include_labels=False, include_types=False)
+        self.compare(alg,
+            f(x),
+            {1: Step(None), 2: Step(ALG.f, 1)}
+        )
 
-        expected = Graph()
-        root = BNode()
-        step_types = [TA.SourceData, TA.TransformedData]
-        steps = [BNode() for _ in step_types]
-
-        expected.add((root, RDF.type, TA.Transformation))
-        for t, n in zip(step_types, steps):
-            expected.add((root, TA.step, n))
-            expected.add((n, RDF.type, t))
-
-        expected.add((steps[1], TA.transformer, ALG.f))
-        expected.add((steps[0], TA.feeds, steps[1]))
-
-        actual = to_isomorphic(actual)
-        expected = to_isomorphic(expected)
-
-        visualize(actual, 'out_actual.dot')
-        visualize(expected, 'out_expected.dot')
-
-        with open('out_expected.dot', 'w') as f:
-            rdf2dot(expected, f)
-
-        # Check that every non-blank node that should be there is there
-        missing = nodes(expected) - nodes(actual)
-        extraneous = nodes(actual) - nodes(expected)
-        self.assertFalse(missing)
-        self.assertFalse(extraneous)
-
-        # If all nodes are there, there must be some wrong connection
-        self.assertEqual(actual, expected)
