a7d696f7709f4f48059a833470df2954c28c4574
Niels Steenbergen <n.steenbergen@uu.nl>
2021-10-01 15:51:03 +0200

    Add Constraint to standard exports.

--- transformation_algebra/__init__.py
+++ transformation_algebra/__init__.py
@@ -1,2 +1,3 @@
-from transformation_algebra.type import Type, operators, _
-from transformation_algebra.expr import TransformationAlgebra, Data, Operation
+from transformation_algebra.type import Type, Constraint, operators, _
+from transformation_algebra.expr import TransformationAlgebra, \
+        Data, Operation, Expr
