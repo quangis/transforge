e6cbc2e2f708b0b4823b20c7fa27ab69002fa02e
Niels Steenbergen <n.steenbergen@uu.nl>
2022-08-21 12:32:59 +0200

    Messed up example repository namespace.

--- transformation_algebra/namespace.py
+++ transformation_algebra/namespace.py
@@ -1,11 +1,11 @@
 import rdflib
 from rdflib import Namespace
 
-TA = Namespace("https://github.com/quangis/transformation-algebra#")
+TA = Namespace('https://github.com/quangis/transformation-algebra#')
 WF = Namespace('http://geographicknowledge.de/vocab/Workflow.rdf#')
 TOOLS = Namespace(
     'https://github.com/quangis/cct/blob/master/tools/tools.ttl#')
 # TOOLS = Namespace('http://geographicknowledge.de/vocab/GISTools.rdf#')
-EX = Namespace('https://example.com/')
+EX = Namespace('https://example.com/#')
 RDF = rdflib.RDF
 RDFS = rdflib.RDFS
