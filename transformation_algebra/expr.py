835560d51ebf38f32fda9780ca5aa21af4df8d1e
Niels Steenbergen <n.steenbergen@uu.nl>
2022-10-14 16:07:57 +0200

    No longer use `~` for printing.

--- transformation_algebra/expr.py
+++ transformation_algebra/expr.py
@@ -158,7 +158,7 @@ class Expr(ABC):
         elif isinstance(self, Source):
             with_type = False
             with_parentheses = False
-            result = f"~{self.type.text()}"
+            result = f"- : {self.type.text()}"
         elif isinstance(self, Application):
             parens = not isinstance(self.f, Application)
             result = f"{self.f.text(labels, with_parentheses=parens)} "\
